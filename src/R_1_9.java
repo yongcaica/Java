7518b8a8764dba105ef8f544e3ccca61cdfcb0da
yongcai <caiyong1@gmail.com>
2019-01-10 11:03:36 -0600

    add

---
+++ src/R_1_9.java
@@ -0,0 +1,65 @@
+/*
+public class R_1_9 {
+    public String trimPunct(String s) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            boolean need_filter = false;
+            char punct[] = {',', '.', '!', '?', ';', ':', '，', '。', '！', '？',
+                    '；', '：', '、'};
+            for (int j = 0; j < punct.length; j++) {
+                if (s.charAt(i) == punct[j]) {
+                    need_filter = true;
+                    break;
+                }
+            }
+            if (!need_filter) {
+                result.append(s.charAt(i));
+            }
+        }
+        return result.toString();
+    }
+
+*/
+
+
+class test {
+    public String trimPunct(String s) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); ++i) {
+            char[] punct = {',', '.', '!', '?', ';', ':', '，', '。', '！', '？',
+                    '；', '：', '、'};
+            boolean need_filter = false;
+            for(char c: punct) {
+                if (c == s.charAt(i)) {
+                    need_filter = true;
+                    break;
+                }
+            }
+            if (!need_filter) {
+                result.append(s.charAt(i));
+            }
+        }
+        return result.toString();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
