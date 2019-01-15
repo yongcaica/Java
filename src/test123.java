830405e4524e1c53fdb5b522e3a285c7b11b90b2
yongcai <caiyong1@gmail.com>
2019-01-15 13:37:27 -0600

    add

---
+++ src/test123.java
@@ -0,0 +1,7 @@
+public class test123{
+    public static void main(String[] args) {
+        int[] arr = {1,2,1};
+        System.out.println(arr[0]);
+//        System.out.println(arr[0] == arr[2]);
+    }
+}
