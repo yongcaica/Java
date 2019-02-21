26967e5c8365c58b90aa6452332061967b9cf6c5
yongcai <caiyong1@gmail.com>
2019-02-20 23:20:23 -0600

    List the problem

--- src/TMA_2_4.java
+++ src/TMA_2_4.java
@@ -1,5 +1,6 @@
 /*
-(10%) Use pseudo-code to describe a nonrecursive method for performing an inorder traversal of a binary tree in linear time.
+(10%) Use pseudo-code to describe a non-recursive method for performing an inorder traversal of a binary tree in
+linear time.
  */
 
 public class TMA_2_4 {
