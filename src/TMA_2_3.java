2caa085394961c23d4e9809918e92426dc861a72
yongcai <caiyong1@gmail.com>
2019-02-20 23:17:24 -0600

    add

--- src/TMA_2_3.java
+++ src/TMA_2_3.java
@@ -1,6 +1,17 @@
 /*
-(10%) Use pseudo-code to describe an algorithm for computing the number of descedents of each node of a binary tree. The algorithm should be based on the Euler tour traversal.
+(10%) Use pseudo-code to describe an algorithm for computing the number of descendents of each node of a binary tree.
+ The algorithm should be based on the Euler tour traversal.
  */
 
-public class TMA_2_3 {
-}
+/*
+Algorithm eulerTourBinary(T, p, num):
+    for each node p in binary tree T do
+        define int count_pre = 0
+        perform the "pre visit" action for position p
+        count_pre++
+        define int count_post = 0
+        perform the "post visit" action for position p
+        count_post++
+        num = Math.abs(count_pre - count_post) + 1
+        num is the number of descendants of p
+ */
