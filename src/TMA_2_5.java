631c360d04ba75f90c1a99793ac318aa9c198a39
yongcai <caiyong1@gmail.com>
2019-02-24 00:23:28 -0600

    add

--- src/TMA_2_5.java
+++ src/TMA_2_5.java
@@ -2,5 +2,8 @@
 (10%) Illustrate the execution of the heap-sort algorithm on the following input sequence: (2, 5, 16, 4, 10, 23, 39, 18, 26, 15).
  */
 
-public class TMA_2_5 {
-}
+/*
+1. We redefine the heap operations to be a maximum-oriented heap, with each
+position key being at least as large as its children.
+
+ */
