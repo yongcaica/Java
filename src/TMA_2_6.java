4ae8753533294fabc2a330f9dbd44dd946b1460e
yongcai <caiyong1@gmail.com>
2019-02-25 23:11:27 -0600

    add

--- src/TMA_2_6.java
+++ src/TMA_2_6.java
@@ -1,3 +1,8 @@
 /*
 (10%) Draw the 11-entry hash table that results from using the hash function h(i)=(3i+5) mod 11, to hash the keys 12, 44, 13, 88, 23, 94, 11, 39, 20, 16, and 5, assuming collisions are handled by chaining.
  */
+
+
+/*
+
+ */
