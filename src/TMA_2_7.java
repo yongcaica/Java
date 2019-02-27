17ae51540e3141b72d3bee88c31c359d44817c57
yongcai <caiyong1@gmail.com>
2019-02-26 23:10:01 -0600

    add

---
+++ src/TMA_2_7.java
@@ -0,0 +1,6 @@
+/*
+(10%) What is the worst-case time for putting n entries in an initially empty hash table, with collisions resolved by chaining? What is the best-case?
+ */
+
+public class TMA_2_7 {
+}
