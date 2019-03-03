68a4570afd7628633c065d93852544d8261c797a
yongcai <caiyong1@gmail.com>
2019-03-02 22:44:02 -0600

    add

--- src/TMA_2_1.java
+++ src/TMA_2_1.java
@@ -1,190 +1,89 @@
-/*
-(20%) Using the Java programming language, implement the deque ADT with a doubly linked list.
- */
 
+class Node<T>{
+    private Node<T> prev;
+    private Node<T> next;
+    private T value;
 
-public class TMA_2_1 {
-}
-
-class Deque<T extends Comparable<T>> {
-    class Node
-    {
-        T data;
-        Node previous;
-        Node next;
-
-        Node(T data)
-        {
-            this.data = data;
-            this.previous = null;
-            this.next = null;
-        }
-
-        Node(T data, Node prev, Node next)
-        {
-            this.data = data;
-            this.previous = prev;
-            this.next = next;
-        }
-
+    public Node<T> getPrev() {
+        return prev;
     }
-    private Node front, back;
-
-    public Deque()
-    {
-        front = null;
-        back = null;
+    public void setPrev(Node<T> prev) {
+        this.prev = prev;
     }
-
-    public boolean isEmpty()
-    {
-        return front == null;
+    public Node<T> getNext() {
+        return next;
     }
-
-    public int size()
-    {
-        int s = 0;
-        Node n = front;
-        while(n != null)
-        {
-            s++;
-            n = n.next;
-        }
-        return s;
+    public void setNext(Node<T> next) {
+        this.next = next;
     }
-
-    public void addFront(T data)
-    {
-        Node n = new Node(data, null, front);
-        if(isEmpty())
-        {
-            front = back = n;
-        }
-        else
-        {
-            front.previous = n;
-            front = n;
-        }
+    public T getValue() {
+        return value;
     }
-
-    public void addBack(T data)
-    {
-        Node n = new Node(data, back, null);
-        if(isEmpty())
-            front = back = n;
-        else
-        {
-            back.next = n;
-            back = n;
-        }
-    }
-
-    public T removeFront()
-    {
-        if(isEmpty())
-            return null;
-
-        T val = front.data;
-
-        front = front.next;
-        if(front == null)
-            back = null;
-        else
-            front.previous = null;
-        return val;
-    }
-
-    public T removeBack()
-    {
-        if(isEmpty())
-            return null;
-
-        T val = back.data;
-
-        back = back.previous;
-        if(back == null)
-            front = null;
-        else
-            back.next = null;
-        return val;
-    }
-
-    public String toString()
-    {
-        String s = "";
-        if(!isEmpty())
-        {
-            s = front.data.toString();
-            Node n = front.next;
-            while(n != null)
-            {
-                s += ", " + n.data.toString();
-                n = n.next;
-            }
-        }
-
-        return s;
-    }
-
-}
-
-/*
-TestDeque.java
-*/
-
-class TestDeque {
-    public static void main(String[] args) {
-        Deque<Integer> deque = new Deque<Integer>();
-
-        System.out.println("addFront 1 2 3 ");
-        deque.addFront(1);
-        deque.addFront(2);
-        deque.addFront(3);
-        System.out.println("Deque contains: " + deque);
-
-        System.out.println("addBack 4 5 6");
-        deque.addBack(4);
-        deque.addBack(5);
-        deque.addBack(6);
-        System.out.println("Deque contains: " + deque);
-
-        System.out.println("size = " + deque.size());
-
-        System.out.println("removeFront : " + deque.removeFront());
-        System.out.println("removeFront : " + deque.removeFront());
-        System.out.println("removeBack : " + deque.removeBack());
-        System.out.println("removeBack : " + deque.removeBack());
-        System.out.println("Deque contains: " + deque);
-
-        System.out.println("size = " + deque.size());
-
+    public void setValue(T value) {
+        this.value = value;
     }
 }
 
-/*
-output
+public class TMA_2_1<T>  {
+    private Node<T> front;
+    private Node<T> rear;
 
-        =====
+    public void insertFront(T item){
+        System.out.println("Adding element at front: "+item);
+        Node<T> nd = new Node<T>();
+        nd.setValue(item);
+        nd.setNext(front);
+        if(front != null) front.setPrev(nd);
+        if(front == null) rear = nd;
+        front = nd;
+    }
 
-        addFront 1 2 3
+    public void insertRear(T item){
+        System.out.println("Adding element at rear: "+item);
+        Node<T> nd = new Node<T>();
+        nd.setValue(item);
+        nd.setPrev(rear);
+        if(rear != null) rear.setNext(nd);
+        if(rear == null) front = nd;
 
-        Deque contains: 3, 2, 1
+        rear = nd;
+    }
 
-        addBack 4 5 6
+    public void removeFront(){
+        if(front == null){
+            System.out.println("Underflow state.");
+            return;
+        }
+        Node<T> tmpFront = front.getNext();
+        if(tmpFront != null) tmpFront.setPrev(null);
+        if(tmpFront == null) rear = null;
+        System.out.println("Remove element from front: "+front.getValue());
+        front = tmpFront;
+    }
 
-        Deque contains: 3, 2, 1, 4, 5, 6
+    public void removeRear(){
+        if(rear == null){
+            System.out.println("Underflow state.");
+            return;
+        }
+        Node<T> tmpRear = rear.getPrev();
+        if(tmpRear != null) tmpRear.setNext(null);
+        if(tmpRear == null) front = null;
+        System.out.println("Removed element from rear: "+rear.getValue());
+        rear = tmpRear;
+    }
 
-        size = 6
-
-        removeFront : 3
-
-        removeFront : 2
-
-        removeBack : 6
-
-        removeBack : 5
-
-        Deque contains: 1, 4
-
-        size = 2
-*/
+    public static void main(String args[]){
+        try {
+            TMA_2_1<Integer> deque = new TMA_2_1<Integer>();
+            deque.insertFront(134);
+            deque.insertFront(14);
+            deque.insertFront(13);
+            deque.removeFront();
+            deque.insertRear(455);
+            deque.removeFront();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
