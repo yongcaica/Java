b8205f1a1fb4568a8664509b4d4ff42da6c80427
yongcai <caiyong1@gmail.com>
2019-03-07 21:19:36 -0600

    newly built question

---
+++ src/TMA_3_8.java
@@ -0,0 +1,11 @@
+/*
+(15%) Let G be a graph whose vertices are the integers 1 through 8, and let the adjacent vertices of each vertex be given by the table below.
+Assume that, in a traversal of G, the adjacent vertices of a given vertex are returned in the same order as they are listed in the table above. You need to:
+Draw graph G. (5%)
+Give the sequence of the vertices of G visited using the depth-first search traversal starting at vertex 1. (5%)
+Give the sequence of vertices visited using the breadth-first search traversal starting at vertex 1. (5%)
+
+ */
+
+public class TMA_3_8 {
+}
