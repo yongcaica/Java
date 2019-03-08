63c5fe9a2c2216e314dbf81da268d869c5187a89
yongcai <caiyong1@gmail.com>
2019-03-07 21:12:57 -0600

    add

---
+++ src/TMA_3_5.java
@@ -0,0 +1,7 @@
+/*
+(10%) Suppose we are given an n-element sequence, S, such that each element in S represents a different vote for president, where each vote is given as an integer representing a particular candidate. Design an O(nlogn)-time algorithm to see who wins the election S represents, assuming the candidate with the most votes wins (even if there are O(n) candidates).
+
+ */
+
+public class TMA_3_5 {
+}
