5a7d2945b800b1f370526d3aac8009e8e0ad8a12
yongcai <caiyong1@gmail.com>
2019-03-07 21:16:37 -0600

    newly built question

---
+++ src/TMA_3_6.java
@@ -0,0 +1,9 @@
+/*
+(10%) Consider the sequence of integers S= {6, 7, 5, 7, 1, 0, 1, 6, 2, 4}. For each of the following sorting algorithms, draw diagrams that trace the execution of the algorithm as it sorts the sequence S.
+Merge-sort
+Quick-sort
+
+ */
+
+public class TMA_3_6 {
+}
